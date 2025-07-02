45335ed3e165e9361bee82abb05fc75453006553
Rahul Durgpal <rahuldurgapal191@gmail.com>
2025-07-02 22:06:05 +0530

    first commit

---
+++ src/main/java/com/example/StudentManagement/repository/StudentRepository.java
@@ -0,0 +1,10 @@
+package com.example.StudentManagement.repository;
+
+import com.example.StudentManagement.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+}
---
+++ src/main/java/com/example/StudentManagement/service/StudentService.java
@@ -0,0 +1,17 @@
+package com.example.StudentManagement.service;
+
+import com.example.StudentManagement.entity.Student;
+
+import java.util.List;
+
+public interface StudentService {
+    List<Student> getAllStudents();
+
+    Student saveStudent(Student student);
+
+    Student getStudentById(Long id);
+
+    Student updateStudent(Student student);
+
+    void deleteStudentById(Long id);
+}
